193bcc9d1a1ee0ec0aec33ed6a6b812ff14f2a80
Unknown <vincent.schrynemakers@gmail.com>
2018-05-24 15:46:14 +0200

    bidule

--- src/main/java/Entities/Partie.java
+++ src/main/java/Entities/Partie.java
@@ -7,13 +7,22 @@ public class Partie {
 	Scanner x = new Scanner(System.in);
 
 	public void lanceIntro() {
-		System.out.println("Ceci est l'intro... \nBienvenue dans 'Cachots & L�zards' \nVotre but est d'aller le plus loin possible pour amasser des points et exploser le score.");
+		System.out.println("Ceci est l'intro... \nBienvenue dans 'Cachots & Lezards' \nVotre but est d'aller le plus loin possible pour amasser des points et exploser le score.");
 	}
 
 	public void creeChoixPortes(Joueur joueur) {
-		Integer portes = 3;	
-		System.out.println("Vous etes face a "+portes+" portes. Laquelle voulez-vous emprunter ?"); 	
-		Integer cj = x.nextInt();
+		Integer cj=0;
+		Integer portes = (int) ((Math.random()*3)+1);
+		do {
+			try {
+				System.out.println("Vous etes face a " + portes + " portes. Laquelle voulez-vous emprunter ?");
+				cj = x.nextInt();
+			} catch (Exception e) {
+				System.out.println("Je n'ai pas compris, recommencez...");
+				Scanner x = new Scanner(System.in);
+				cj = x.nextInt();
+			} 
+		} while (cj<1 || cj>portes);
 		Integer de = (int) ((Math.random()*5)+1);
 		switch(de) {
 		case 1: PieceVide pv=new PieceVide(); pv.generatePieceVide(joueur);break;
@@ -25,7 +34,7 @@ public class Partie {
 	}
 
 	public void finPartie() {
-		System.out.println("La partie est termin�e");	
+		System.out.println("La partie est terminee");	
 	}
 
 }
