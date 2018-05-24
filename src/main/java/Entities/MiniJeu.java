2212b034b35c0b7503a8b7bae373afbb9bfe0842
Unknown <jpcatel1979@gmail.com>
2018-05-24 14:59:40 +0200

    for vincent

--- src/main/java/Entities/MiniJeu.java
+++ src/main/java/Entities/MiniJeu.java
@@ -2,7 +2,9 @@ package Entities;
 //// cette class va cr�er et lancer les mini jeux en fonction d'un num�ros
 public class MiniJeu {
 	
-	public boolean lauchMiniJeu(Integer miniJeu){
+	public boolean lauchMiniJeu(){
+		//Integer miniJeu = new Integer((int) (Math.random() * (3 + 1 - 0) + 3));
+		Integer miniJeu = 3;
 		System.out.println("Lancement du mini jeu N "+miniJeu);
 		switch (miniJeu){
 /*		case 1 : 
@@ -14,6 +16,8 @@ public class MiniJeu {
 			MiniJeu2 game2 = new MiniJeu2();
 			return game2.lancer();
 			*/
+		case 1 :
+		case 2 :
 		case 3 : 
 			MiniJeu3 game3 = new MiniJeu3();
 			return game3.lancer();
