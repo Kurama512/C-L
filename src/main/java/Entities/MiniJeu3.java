ad139cafa951a4ca1ebb10ee0fa4e9a9f0b32b8b
Unknown <jpcatel1979@gmail.com>
2018-05-24 14:40:03 +0200

    mise à jour miniJeu3

--- src/main/java/Entities/MiniJeu3.java
+++ src/main/java/Entities/MiniJeu3.java
@@ -3,21 +3,22 @@ package Entities;
 import java.util.Scanner;
 
 public class MiniJeu3 {
-	Integer min;
-	Integer max;
-	Integer essaie;
-	Integer justePrix;
+	private Integer min;
+	private Integer max;
+	private Integer essaie;
+	private Integer justePrix;
+	private Integer choix;
+	private Boolean find;
 
 	public MiniJeu3() {
 		this.min = 0;
 		this.max = 100;
 		this.essaie = 8;
 		this.justePrix = new Integer((int) (Math.random() * (max + 1 - min) + max));
+		find = false;
 	}
 
 	public boolean lancer() {
-		Boolean find = false;
-		Integer choix;
 		while (!find && essaie > 0) {
 			choix = choixJoueur();
 
@@ -27,20 +28,17 @@ public class MiniJeu3 {
 
 			if (choix > justePrix) {
 				System.out.println("Plus petit");
-
 			}
 
 			if (choix < justePrix) {
 				System.out.println("Plus grand");
 			}
-
 			essaie--;
 
 		}
-		if(find){
+		if (find) {
 			System.out.println("Vous avez gagné");
-		}
-		else{
+		} else {
 			System.out.println("Vous avez perdu");
 		}
 		return find;
@@ -48,14 +46,69 @@ public class MiniJeu3 {
 	}
 
 	public Integer choixJoueur() {
-		System.out.println("Choissisez un nombre entre " + min + " et " + max);
+		System.out.println("Choisissez un nombre entre " + min + " et " + max);
 		Scanner choixClavier = new Scanner(System.in);
-		Integer choix = new Integer(choixClavier.nextInt());
-		if (choix < min && choix > max) {
+		choix = new Integer(choixClavier.nextInt());
+
+		if (!choixCorrecte(choix)) {
 			System.out.println("Votre nombre " + choix + " n'est pas dans l'interval requis");
 			return choixJoueur();
 		}
 		return choix;
+	}
+
+	public Boolean choixCorrecte(Integer choix) {
+		if (choix < this.getMin() || choix > this.getMax())
+			return false;
+		return true;
+	}
+
+	public Integer getMin() {
+		return min;
+	}
+
+	public void setMin(Integer min) {
+		this.min = min;
+	}
+
+	public Integer getMax() {
+		return max;
+	}
+
+	public void setMax(Integer max) {
+		this.max = max;
+	}
+
+	public Integer getEssaie() {
+		return essaie;
+	}
+
+	public void setEssaie(Integer essaie) {
+		this.essaie = essaie;
+	}
+
+	public Integer getJustePrix() {
+		return justePrix;
+	}
+
+	public void setJustePrix(Integer justePrix) {
+		this.justePrix = justePrix;
+	}
+
+	public Integer getChoix() {
+		return choix;
+	}
+
+	public void setChoix(Integer choix) {
+		this.choix = choix;
+	}
+
+	public Boolean getFind() {
+		return find;
+	}
+
+	public void setFind(Boolean find) {
+		this.find = find;
 	}
 
 }
