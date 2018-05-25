91ccfc00f8c636f5710e057e102c855e3d4c0224
K512 <vincent.schrynemakers@gmail.com>
2018-05-25 13:48:20 +0200

    Auto stash before merge of "develop" and "C-L/develop"

--- src/main/java/Entities/MiniJeu.java
+++ src/main/java/Entities/MiniJeu.java
@@ -14,7 +14,7 @@ public class MiniJeu {
 		case 2 : 
 			System.out.println("La gigantesque pièce dans laquelle vous vous trouvez est en réalité \nune fosse hérissée de pics. Un seul chemin mène aux prochaines portes : un pont branlant...\n");
 			MiniJeu2 game2 = new MiniJeu2();
-			return game2.lancer();
+			return game2.lancerMiniJeu2();
 			
 		case 3 : 
 			System.out.println("A votre grande surprise, la pièce ne contient aucune porte.\nUn levier est situé derrière une grille fermée par un cadenas incassable.\nSi vous voulez avancer, vous allez devoir en trouver la combinaison,\nmais ne tardez pas, l'alarme a déjà été sonnée...\n");
